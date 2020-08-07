9bd16c9dede2df71f3827bc4667f1db3b9443e06
Vic <Vic@gmail.com>
2020-08-07 17:51:01 +0800

    初始化

---
+++ src/views/layout/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <section class="container">
+      <div class="row">
+        <div class="col">
+          <h2>{{title}}</h2>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      title: 'JS特訓班'
+    }
+  }
+}
+</script>
---
+++ src/views/layout/Layout.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <nav id="nav" class="navbar navbar-expand-lg navbar-light font-weight-bold">
+      <div class="container">
+        <router-link to="/" class="navbar-brand text-primary">
+          <h1 class="h2 d-flex align-items-center">
+            Vic
+          </h1>
+        </router-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link to="/checkpoints" class="nav-link">40~60關</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
+</template>
